26754f6b9b3fcdcb49c5396c8678be392cb87221
Sven van Straalen <ssgvanstraalen@gmail.com>
2022-01-28 21:03:07 +0100

    try without git

--- bootup.sh
+++ bootup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-git -C /home/pi/devastatorTank checkout main
-git -C /home/pi/devastatorTank pull
+
+#git -C /home/pi/devastatorTank checkout main
+#git -C /home/pi/devastatorTank pull
 /usr/bin/python3 /home/pi/devastatorTank/main.py
